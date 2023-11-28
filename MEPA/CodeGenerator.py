c4f086a0463a50b8e2929eee568f119d41300fdd
G-Nozawa <gabriel.nozawa@unesp.br>
2023-11-28 16:26:38 -0300

    finalizando a geracao de codigo

--- MEPA/CodeGenerator.py
+++ MEPA/CodeGenerator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Optional
+from LangGrammar import LangGrammar
 from tools.LangGrammar import LangGrammar
 from tools.LangGrammarVisitor import LangGrammarVisitor
 
@@ -12,27 +13,14 @@ class Scope:
     def __init__(self, scope_name: str, enclosing_scope: Optional[Scope] = None):
         self.scope_name = scope_name
         self.enclosing_scope: Optional[Scope] = enclosing_scope
-        self.symbols = {}
+        self.symbols: dict[str, Symbol] = {}
 
     def define(self, symbol: Symbol):
         self.symbols[symbol.name] = symbol
 
     def resolve(self, name: str) -> Optional[Symbol]:
         return self.symbols.get(name)
-    
-# Objetivos da análise semântica:
-# * Variável ou procedimento não declarado
-# * Variável ou procedimento declarado mais de uma vez
-# * Incompatibilidade de parâmetros formais e reais: número, ordeme tipo
-# * Uso de variáveis variáveis de escopo inadequado
-# * Atribuição de um inteiro a um booleano
-# * Divisão que não é entre números inteiros
-# * Variável declarada e nunca utilizada
-# * Read e write com variáveis de tipo diferentes
-# * Tratamento de escopo
-#   - Erro: variável local a um procedimento utilizada no programa principa
 
-# Esta classe herda e sobrescreve o visitor do ANTLR para fazer a análise semântica
 class CodeGenerator(LangGrammarVisitor):
     
     def __init__(self, errorListener):
@@ -52,130 +40,197 @@ class CodeGenerator(LangGrammarVisitor):
     def visitPrograma(self, ctx: LangGrammar.ProgramaContext):
         self.generated_code.append("INPP\n")
         self.visitChildren(ctx)
-        self.generated_code("PARA\n")
+        self.generated_code.append("PARA\n")
         self.salvarPrograma()
 
-    # Variável não declarada
-    # Variáveis só aparecem em dois contextos, além de sua declaração:
-    # Atribuição (atribuicao) e fator
-
-    # Visit a parse tree produced by LangGrammar#atribuicao.
-    def visitAtribuicao(self, ctx:LangGrammar.AtribuicaoContext):
-        variavel = ctx.variavel()
-        assert variavel is not None
-        nome_variavel = variavel.IDENTIFICADOR().getText()
-        symbol = self.current_scope.resolve(nome_variavel)
-        return self.visitChildren(ctx)
-
     def visitListaIdentificadores(self, ctx: LangGrammar.ListaIdentificadoresContext):
-        self.generated_code.append("AMEM 1\n")
-        variavel = ctx.variavel()
-        nome_variavel = variavel.IDENTIFICADOR().getText()
-        self.current_scope.define(Symbol(self.current_scope.resolve(nome_variavel), len(self.current_scope.symbols)))
-        return self.visitChildren(ctx)
+        id = ctx.IDENTIFICADOR()
+        if(id != None):
+            nome_variavel = id.getText()
+            pos_stack = int(len(self.current_scope.symbols))
+            print("nome_variavel: ", nome_variavel)
+            print("posicao: ", pos_stack)
+            symbol = Symbol(nome_variavel, pos_stack)
+            self.current_scope.define(symbol)
+            self.generated_code.append("AMEM 1\n")
+        self.visitChildren(ctx)
 
     def visitListaIdentificadores1(self, ctx: LangGrammar.ListaIdentificadoresContext):
-        self.generated_code.append("AMEM 1\n")
-        variavel = ctx.variavel()
-        nome_variavel = variavel.IDENTIFICADOR().getText()
-        self.current_scope.define(Symbol(self.current_scope.resolve(nome_variavel), len(self.current_scope.symbols)))
-        return self.visitChildren(ctx)
+        id = ctx.IDENTIFICADOR()
+        if(id != None):
+            nome_variavel = id.getText()
+            pos_stack = int(len(self.current_scope.symbols))
+            print("posicao: ", pos_stack)
+            symbol = Symbol(nome_variavel, pos_stack)
+            self.current_scope.define(symbol)
+            self.generated_code.append("AMEM 1\n")
+        self.visitChildren(ctx)
 
+    def visitExpressao1(self, ctx: LangGrammar.Expressao1Context):
+        relacao = ctx.relacao()
+        if(relacao != None):
+            print(len(self.generated_code))
+            # self.visitChildren(ctx.expressaoSimples())
+            # self.visitChildren(ctx.relacao())
+            self.visitChildren(ctx)
+            if(relacao.EQUAL() != None):
+                print("EQ")
+                self.generated_code.append("CMIG\n")
+            elif(relacao.DIFF() != None):
+                print("DIF")
+                self.generated_code.append("CMDG\n")
+            elif(relacao.LT() != None):
+                print("MEN")
+                self.generated_code.append("CMME\n")
+            elif(relacao.LTE() != None):
+                print("MEQ")
+                self.generated_code.append("CMEG\n")
+            elif(relacao.GT() != None):
+                print("MAI")
+                self.generated_code.append("CMMA\n")
+            elif(relacao.GTE() != None):
+                print("MAQ")
+                self.generated_code.append("CMAG\n")
+            
+
+    # def visitRelacao(self, ctx:LangGrammar.RelacaoContext ):
+    #     if(ctx.EQUAL() != None):
+    #         print("EQ")
+    #         self.generated_code.append("CMIG\n")
+    #     elif(ctx.DIFF() != None):
+    #         print("DIF")
+    #         self.generated_code.append("CMDG\n")
+    #     elif(ctx.LT() != None):
+    #         print("MEN")
+    #         self.generated_code.append("CMME\n")
+    #     elif(ctx.LTE() != None):
+    #         print("MEQ")
+    #         self.generated_code.append("CMEG\n")
+    #     elif(ctx.GT() != None):
+    #         print("MAI")
+    #         self.generated_code.append("CMMA\n")
+    #     elif(ctx.GTE() != None):
+    #         print("MAQ")
+    #         self.generated_code.append("CMAG\n")
 
     def visitExpressaoSimples(self, ctx:LangGrammar.ExpressaoSimplesContext ):
-        self.visitChildren(ctx.getChild(0))
         if(ctx.SUB() != None):
-            self.generated_code.append("INVR\n")
-        self.visitChildren(ctx.getChild(1))
-        return
+            if(type(ctx.parentCtx.parentCtx) == LangGrammar.Variavel1Context):
+                self.visitChildren(ctx)
+                self.generated_code.append("SUBT\n")
+            else:
+                instruction_reminder = len(self.generated_code)
+                self.visitChildren(ctx.termo())
+                self.generated_code.insert(instruction_reminder+1, "INVR\n")
+                self.visitChildren(ctx.expressaoSimples1())
+        elif(ctx.SUM() != None):
+            self.visitChildren(ctx)
+            self.generated_code.append("SOMA\n")
+        else:
+            self.visitChildren(ctx)
 
     def visitExpressaoSimples1(self, ctx:LangGrammar.ExpressaoSimples1Context ):
         self.visitChildren(ctx)
         if(ctx.SUB() != None):
-            self.generated_code.append("SUB\n")
+            self.generated_code.append("SUBT\n")
         elif(ctx.SUM() != None):
-            self.generated_code.append("SUM\n")
+            self.generated_code.append("SOMA\n")
         elif(ctx.OR() != None):
             self.generated_code.append("DISJ\n")
-        return
 
     def visitFator(self, ctx: LangGrammar.FatorContext):
         if(ctx.variavel() != None):
             #verificar como pegar a posicao da variavel no stack de dados
             variavel = ctx.variavel()
-            nome_variavel = variavel.IDENTIFICADOR().getText()
+            id = variavel.IDENTIFICADOR()
+            nome_variavel = id.getText()
+            self.historico_variavel.append(nome_variavel)
             stack_position = self.current_scope.resolve(nome_variavel).pos_stack
-            self.generated_code.append(f"CRVR {stack_position}")
+            self.generated_code.append(f"CRVR {stack_position}\n")
             # pass
         elif(ctx.numero() != None):
-            num = ctx.numero()
-            self.generated_code.append(f"CRVL {num}")
+            num = int(ctx.numero().getText())
+            self.generated_code.append(f"CRVL {num}\n")
         #TODO verificar como representar booleanos na MEPA
         elif(ctx.CONST_FALSE() != None):
-            self.generated_code.append(f"CRVL 0")
+            self.generated_code.append(f"CRVL 0\n")
         elif(ctx.CONST_TRUE() != None):
-            self.generated_code.append("CRVL 1")
+            self.generated_code.append("CRVL 1\n")
         self.visitChildren(ctx)
         if(ctx.NOT() != None):
-            self.generated_code.append("NEGA")
+            self.generated_code.append("NEGA\n")
         
     #TODO
     def visitTermo1(self, ctx: LangGrammar.Termo1Context):
         #resolve primeiro o fator e depois resolve o termo
-        self.visitChildren(ctx.getChild(0))
+        if(ctx.children != None):
+            self.visitChildren(ctx)
 
-        if(ctx.MUL() != None):
-            self.generated_code.append(f"MULT")
+            if(ctx.MUL() != None):
+                self.generated_code.append(f"MULT\n")
 
-        elif(ctx.INT_DIV() != None):
-            self.generated_code.append(f"DIVI")
+            elif(ctx.INT_DIV() != None):
+                self.generated_code.append(f"DIVI\n")
 
-        elif(ctx.AND() != None):
-            self.generated_code.append(f"CONJ")
-        
-        self.visitChildren(ctx.getChild(1))
+            elif(ctx.AND() != None):
+                self.generated_code.append(f"CONJ\n")
+            
 
     def visitComandoRepetitivo1(self, ctx:LangGrammar.ComandoRepetitivo1Context):
-        inicio_laco = len(self.generated_code()) - 1
-        self.generated_code.append("NADA")
+        inicio_laco = len(self.generated_code)
+        self.generated_code.append("NADA\n")
         #visita a expressao e adiciona um placeholder para indicar o final do bloco de repeticao
-        self.visitChildren(ctx.getChild(0))
-        instruction_reminder = len(self.generated_code())
+        self.visitChildren(ctx.expressao())
+        instruction_reminder = len(self.generated_code)
         #adicionei placeholder ao inves de vazio para debug
-        self.generated_code.append("PLACEHOLDER COMANDO REPETITIVO")
-        self.visitChildren(ctx.getChild(1))
-        self.generated_code.append(f"DSVS {inicio_laco}")
-        self.generated_code[instruction_reminder] = f"DSVF {instruction_reminder}"
+        self.generated_code.append("PLACEHOLDER COMANDO REPETITIVO\n")
+        self.visitChildren(ctx.comando())
+        self.generated_code.append(f"DSVS {inicio_laco}\n")
+        self.generated_code[instruction_reminder] = f"DSVF {len(self.generated_code)}\n"
+        self.generated_code.append("NADA\n")
 
     def visitAtribuicao(self, ctx: LangGrammar.AtribuicaoContext):
         variavel = ctx.variavel()
-        nome_variavel = variavel.IDENTIFICADOR().getText()
+        id = variavel.IDENTIFICADOR()
+        nome_variavel = id.getText()
         self.historico_variavel.append(nome_variavel)
         symbol = self.current_scope.resolve(nome_variavel)
         num = symbol.pos_stack
         self.visitChildren(ctx)
-        self.generated_code.append(f"ARMZ {num}")
+        self.generated_code.append(f"ARMZ {num}\n")
 
     def visitComandoCondicional(self, ctx: LangGrammar.ComandoCondicionalContext):
-        self.visitChildren(ctx.getChild(0))
-        instruction_reminder = len(self.generated_code())
-        self.generated_code.append("PLACEHOLDER COMANDO CONDICIONAL")
+        self.visitChildren(ctx.expressao())
+        instruction_reminder = len(self.generated_code)
+        self.generated_code.append("PLACEHOLDER COMANDO CONDICIONAL\n")
         #armazena todas as instrucoes caso verdadeiro
-        self.visitChildren(ctx.getChild(1))
+        self.visitChildren(ctx.comando())
         #retorna a instrucao placeholder e substituição pelo valor de instrução atual
-        self.generated_code[instruction_reminder] = f"DSVF {len(self.generated_code)}"
-        self.generated_code.append("NADA")
+        self.generated_code[instruction_reminder] = f"DSVF {len(self.generated_code)}\n"
+        self.generated_code.append("NADA\n")
+        instruction_reminder = len(self.generated_code)
+        self.visitChildren(ctx.comandoCondicional1())
+        current_instruction = len(self.generated_code)
+        if(instruction_reminder != current_instruction):
+            self.generated_code.insert(instruction_reminder, f"DSVS {current_instruction+1}\n")
+            self.generated_code.append("NADA\n")
 
     def visitChamadaProcedimento(self, ctx: LangGrammar.ChamadaProcedimentoContext):
-        if(ctx.PROC_READ):
-            self.generated_code.append("LEIT")
-            for variavel in self.historico_variavel:
-                stack_position = self.current_scope.resolve(variavel).pos_stack
-                self.generated_code.append(f"ARMZ {stack_position}")
-
-        elif(ctx.PROC_WRITE):
+        if(ctx.PROC_READ() != None):
+            new_instruction_pointer = len(self.generated_code)
             self.visitChildren(ctx)
-            self.generated_code.append("IMPR")
+            #descarta as operacoes inuteis
+            while(new_instruction_pointer != len(self.generated_code)):
+                self.generated_code.pop()
+            for variavel in self.historico_variavel:
+                #permite a entrada de multiplas variaveis numa mesma chamada
+                self.generated_code.append("LEIT\n")
+                stack_position = self.current_scope.resolve(variavel).pos_stack
+                self.generated_code.append(f"ARMZ {stack_position}\n")
+
+        elif(ctx.PROC_WRITE() != None):
+            self.visitChildren(ctx)
+            self.generated_code.append("IMPR\n")
 
     #lista para leitura de variavel
     def visitListaExpressao(self, ctx: LangGrammar.ListaExpressaoContext):
