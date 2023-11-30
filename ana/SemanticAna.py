2c7c0b1d8814bb21b1152d4aa6df9a947b70833e
Salies <serezane@protonmail.com>
2023-11-30 13:22:17 -0300

    feat semântica: escopo

--- ana/SemanticAna.py
+++ ana/SemanticAna.py
@@ -30,7 +30,7 @@ class Scope:
 # * Variável declarada e nunca utilizada
 # * Read e write com variáveis de tipo diferentes
 # * Tratamento de escopo
-#   - Erro: variável local a um procedimento utilizada no programa principa
+#   - Erro: variável local a um procedimento utilizada no programa principal
 
 # Esta classe herda e sobrescreve o visitor do ANTLR para fazer a análise semântica
 class SemanticAna(LangGrammarVisitor):
@@ -41,24 +41,33 @@ class SemanticAna(LangGrammarVisitor):
 
 
     def enterScope(self, scope_name: str):
+        print('entrei no escopo', scope_name    )
         scope = Scope(scope_name, self.current_scope)
         self.current_scope = scope
 
 
     def exitScope(self):
+        print('sai do escopo')
         if self.current_scope.enclosing_scope is not None:
             self.current_scope = self.current_scope.enclosing_scope
+
+    # A mudança de escopo ocorre quando se entra em um procedimento
+    def visitDeclaracaoProcedimento(self, ctx: LangGrammar.DeclaracaoProcedimentoContext):
+        nome_procedimento = ctx.IDENTIFICADOR().getText()
+        self.enterScope(nome_procedimento)
+        self.visitChildren(ctx)
+        self.exitScope()
 
     # Variável não declarada
     # Variáveis só aparecem em dois contextos, além de sua declaração:
     # Atribuição (atribuicao) e fator
-
-    # Visit a parse tree produced by LangGrammar#atribuicao.
     def visitAtribuicao(self, ctx:LangGrammar.AtribuicaoContext):
         variavel = ctx.variavel()
         assert variavel is not None
         nome_variavel = variavel.IDENTIFICADOR().getText()
         symbol = self.current_scope.resolve(nome_variavel)
         if symbol is None:
-           return self.errorListener.addError(f"Variável '{nome_variavel}' não declarada.", variavel.start.line, variavel.start.column)
+           err = f"Variável '{nome_variavel}' não declarada."
+           print(err)
+           return self.errorListener.addError(err, variavel.start.line, variavel.start.column)
         #return self.visitChildren(ctx)
